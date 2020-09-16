496b64c92f7a9fc0e4d5b8013602e25cb847f2ec
IRG <IRG@Rehan-PC>
2020-09-16 10:03:25 +0530

    Make changes in A class ny Rehan

--- GitsDemoProject/src/com/cjc/A.java
+++ GitsDemoProject/src/com/cjc/A.java
@@ -1,6 +1,11 @@
 package com.cjc;
 
 public class A {
+	
+	public void m1() {
+		System.out.println("This is m1-methd");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Demo for gits");
 	}

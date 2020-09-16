e09332b7dd357d81815187add2da726cfff9938d
mohammadrehan1 <71365783+mohammadrehan1@users.noreply.github.com>
2020-09-16 17:41:49 +0530

    Update GitzDemo.java
    Added one method that is m1() method in GitzDemo2

--- GitsDemo2/src/com/cjc/GitzDemo.java
+++ GitsDemo2/src/com/cjc/GitzDemo.java
@@ -1,6 +1,11 @@
 package com.cjc;
 
 public class GitzDemo {
+	
+	
+	public void m1() {
+	System.out.println("Gitz Demo2 m1 method");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Gitz Demo main method");

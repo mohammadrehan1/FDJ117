16a4ccbde37c248968f48d519612ef2918aa5610
IRG <IRG@Rehan-PC>
2020-09-16 13:22:24 +0530

    Currently add new sop statement

--- GitsDemoProject/src/com/cjc/A.java
+++ GitsDemoProject/src/com/cjc/A.java
@@ -13,6 +13,9 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("Demo for gits");
 		A a=new A();
+		a.m1();
+		System.out.println("this is main method");
+		
 	}
 
 }
